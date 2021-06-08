244ac0b14321363372525949c92a6b29047cd4e5
Greg Huang <gregoryhuang2005@gmail.com>
2021-06-08 00:46:02 -0400

    Change structure to add reproducibility to code; will edit again once June 8 data is released.

--- tabulizer.R
+++ tabulizer.R
@@ -3,29 +3,116 @@ install.packages("tabulizer")
 library(tabulizer)
 library(tidyverse)
 
-#### Setup Master #####
-master <- extract_tables("june5.pdf")
-page1 <- as.data.frame(master[[1]], stringAsFactors = FALSE)
-page2 <- as.data.frame(master[[2]], stringAsFactors = FALSE)
-master <- bind_rows(page1, page2)
-master <- master[-1,1:11]
-colnames(master) <- c("CaseNum","Gender","Age","Chronic","History",
-                           "SymptomDate", "Symptoms","TestDate",
-                           "QuarantineDate","ConfirmDate","DOD")
-deaths <- as.data.frame(table(master$DOD))
-colnames(deaths) <- c("Date", "Deaths")
+##### Retrospective Data Additions #####
+# May 28 to June 4th all have the same format
+# Loop through 8 documents in retro_pdf
 
-##### Daily Updates #####
-today <- extract_tables("june7.pdf")
-page1_today <- as.data.frame(today[[1]], stringAsFactors = FALSE)
-
-if (length(today) > 1){
-  for (x in 2:length(today)){
-    nextPage <- as.data.frame(today[[x]], stringAsFactors = FALSE)
-    nextPage <- nextPage[-1,]
-    page1_today <- bind_rows(page1_today,nextPage)
+index <- 0
+for (item in list.files("retro_pdf")){
+  path <- paste("retro_pdf/",item, sep = "")
+  pdf_table <- extract_tables(path) 
+  if (index == 0){
+    master <- as.data.frame(pdf_table[[1]], stringAsFactors = FALSE)
+    master <- master[-1,]
+    index <- index + 1
+  }
+  else{
+    pdf_table <- as.data.frame(pdf_table[[1]], stringAsFactors = FALSE)
+    pdf_table <- pdf_table[-1,1:11]
+    master <- bind_rows(master, pdf_table)
   }
 }
+
+
+##### Setup June 5th (unique case w/ locations) #####
+june5 <- extract_tables("june5.pdf")
+page1 <- as.data.frame(june5[[1]], stringAsFactors = FALSE)
+page2 <- as.data.frame(june5[[2]], stringAsFactors = FALSE)
+june5 <- bind_rows(page1, page2)
+june5 <- june5[-1,1:11]
+master <- bind_rows(master,june5)
+colnames(master) <- c("CaseNum","Gender","Age","Chronic","History",
+                      "SymptomDate", "Symptoms","TestDate",
+                      "QuarantineDate","ConfirmDate","DOD")
+
+##### June 6 onward #####
+#Starting from June 6th, the pdf format changes again. 
+
+for(item in list.files("pdf")){
+  path <- paste("pdf/",item, sep = "")
+  pdf_table <- extract_tables(path)   
+  page1 <- as.data.frame(pdf_table[[1]], stringAsFactors = FALSE)
+  page1 <- page1[-1,2:12] 
+  
+  if(length(pdf_table) > 1) {
+    for (x in 1:length(pdf_table)){
+      nextPage <- as.data.frame(pdf_table[[x]], stringAsFactors = FALSE)
+      nextPage <- nextPage[-1,2:12]
+      final <- bind_rows(page1,nextPage)
+      }
+  }
+  else{
+    final <- page1
+  }
+  colnames(final) <- c("CaseNum","Gender","Age","Chronic","History",
+                        "SymptomDate", "Symptoms","TestDate",
+                        "QuarantineDate","ConfirmDate","DOD")
+  master <- bind_rows(master,final)
+  
+}
+
+##### Analytics for MASTER data #####
+analytics <- master %>%
+  mutate(Age = paste("Age_", Age, sep=""))%>%
+  mutate(Age = str_sub(Age,1,6)) %>%
+  mutate(Age = paste(Age,"+",sep="")) %>%
+  mutate(Chronic = str_replace(Chronic, "無", "No_Chronic_Condition")) %>%
+  mutate(Chronic = str_replace(Chronic, "調查中",
+                               "Chronic_Condition_Uncertain")) %>%
+  mutate(Chronic = iconv(Chronic,from = "latin1", to = "ASCII")) %>%
+  mutate(Chronic = replace_na(Chronic, "Has_Chronic_Condition")) %>%
+  mutate(DOD = as.character(DOD))
+
+
+deaths <- as.data.frame(table(analytics$DOD))
+colnames(deaths) <- c("Date", "Deaths")
+deaths <- deaths %>%
+  mutate(Date = as.character(Date)) %>%
+  arrange(Date)
+  
+gender_tally <- analytics %>%
+  select(DOD, Gender) %>%
+  count(Gender,DOD) %>%
+  spread(Gender, n) %>%
+  arrange(DOD) %>%
+  select(-DOD)
+
+chronic_tally <- analytics %>%
+  count(Chronic, DOD) %>%
+  spread(Chronic, n) %>%
+  arrange(DOD) %>%
+  select(-DOD)
+
+age_tally <- analytics %>%
+  count(Age, DOD) %>%
+  spread(Age, n) %>%
+  arrange(DOD) %>%
+  select(-DOD)
+
+analytics_release <- bind_cols(list(deaths, gender_tally,
+                                    chronic_tally, age_tally))
+colnames(analytics_release)[colnames(analytics_release) == "女"] <- "Female"
+colnames(analytics_release)[colnames(analytics_release) == "男"] <- "Male"
+analytics_release[is.na(analytics_release)] <- 0
+
+write.csv(analytics_release, "Full_statistics.csv")
+
+# Code above allows for reproduction of the entire database up til 
+# the most recent press release of death counts. 
+
+##### DAILY CHANGE TRACKING #####
+today <- extract_tables("june8.pdf")
+
 master_today <- page1_today[-1,2:12]
 colnames(master_today) <- c("CaseNum","Gender","Age","Chronic","History",
                          "SymptomDate", "Symptoms","TestDate",
@@ -36,7 +123,7 @@ deaths <- as.data.frame(table(master$DOD))
 colnames(deaths) <- c("Date", "Deaths")
 master <- bind_rows(master, master_today)
 
-##### Analytics #####
+
 deaths_update <- as.data.frame(table(master$DOD))
 colnames(deaths_update) <- c("Date", "Deaths")
 
@@ -51,30 +138,6 @@ compare <- full_join(deaths, deaths_update, by = "Date",
   arrange(Date)
 
 
-chronic_tally <- master %>%
-  select(DOD, Chronic) %>%
-  mutate(DOD = as.character(DOD)) %>%
-  count(Chronic, DOD) %>%
-  spread(Chronic, n) %>%
-  arrange(DOD) %>%
-  select(-DOD)
-
-gender_tally <- master %>%
-  select(DOD, Gender) %>%
-  mutate(DOD = as.character(DOD))%>%
-  count(Gender,DOD) %>%
-  spread(Gender, n) %>%
-  arrange(DOD) %>%
-  bind_cols(chronic_tally)
-
-release <- bind_cols(compare, gender_tally) %>%
-  select(-DOD) %>%
-  select(-Deaths_reported_yesterday)
-  
-colnames(release) <- c("Date_of_Death",
-                      "Updated_Death_Count", "New_Deaths_Reported_Today", 
-                      "Female", "Male", "Has_Chronic_Illness", 
-                      "No_Chronic_Illness", "Chronic_Illness_Uncertain")
 release[is.na(release)]<- 0
 
 
